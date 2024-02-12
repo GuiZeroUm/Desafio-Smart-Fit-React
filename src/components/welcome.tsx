7b95e22788389ff7d138269838f20e4c39711072
Guilherme Santos <110872956+GuiZeroUm@users.noreply.github.com>
2024-02-11 20:46:43 -0500

    feat: add Welcome components

---
+++ src/components/welcome.tsx
@@ -0,0 +1,14 @@
+const Welcome = () => {
+    return ( 
+    <div className="py-12 px-5">
+        <h1 className="text-4xl font-bold uppercase mb-6">Reabertura<br/>Smart Fit</h1>
+
+        <div className="h-4 w-36 bg-brand-darkGrey mb-8"></div>
+
+        <p>
+        O horário de funcionamento das nossas unidades está seguindo os decretos de cada município. Por isso, confira aqui se a sua unidade está aberta e as medidas de segurança que estamos seguindo.
+        </p>
+    </div> );
+}
+ 
+export default Welcome;
